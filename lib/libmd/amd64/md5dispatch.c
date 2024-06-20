c131841d4d11cc590adf8680d41c9cb3dd27cbc5
Robert Clausecker <fuz@FreeBSD.org>
2024-06-20 09:50:43 -0400

    lib/libmd/amd64/md5block.S: add AVX-512 implementation
    This implementation uses AVX-512 to leverage the VPTERNLOGD instruction,
    allowing us to compute the mixing function in one step.  There's not
    really any vectorisation going on though.
    
    pre     17.0s (602 MB/s)
    generic 18.8s (545 MB/s)
    scalar  13.4s (764 MB/s)
    bmi1    12.0s (853 MB/s)
    avx512  10.6s (966 MB/s)

--- lib/libmd/amd64/md5dispatch.c
+++ lib/libmd/amd64/md5dispatch.c
@@ -12,9 +12,13 @@
 
 extern void md5block_baseline(MD5_CTX *, const void *, size_t);
 extern void md5block_bmi1(MD5_CTX *, const void *, size_t);
+extern void md5block_avx512(MD5_CTX *, const void *, size_t);
 
 DEFINE_UIFUNC(, void, md5block, (MD5_CTX *, const void *, size_t))
 {
+	if ((cpu_stdext_feature & (CPUID_STDEXT_AVX512F | CPUID_STDEXT_AVX512VL))
+	    == (CPUID_STDEXT_AVX512F | CPUID_STDEXT_AVX512VL))
+		return (md5block_avx512);
 	if (cpu_stdext_feature & CPUID_STDEXT_BMI1)
 		return (md5block_bmi1);
 	else
