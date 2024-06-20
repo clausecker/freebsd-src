c4a0593ed6be1591a70f0ab46cb5a861da4f957f
Robert Clausecker <fuz@FreeBSD.org>
2024-06-20 09:50:43 -0400

    lib/libmd/amd64/md5block.S: add AVX-512 implementation
    This implementation uses AVX-512 to leverage the VPTERNLOGD instruction,
    allowing us to compute the mixing function in one step.  There's not
    really any vectorisation going on though.
    
    The code is not enabled in the kernel as it would require turning on the
    AVX-512 unit, which is likely too costly.
    
    pre     17.0s (602 MB/s)
    generic 18.8s (545 MB/s)
    scalar  13.4s (764 MB/s)
    bmi1    12.0s (853 MB/s)
    avx512  10.6s (966 MB/s)

--- sys/crypto/md5/md5dispatch_amd64.c
+++ sys/crypto/md5/md5dispatch_amd64.c
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-// #include <stdint.h>
 #include <machine/specialreg.h>
 #include <sys/types.h>
 #include <sys/md5.h>
@@ -12,9 +11,21 @@
 
 extern void _libmd_md5block_baseline(MD5_CTX *, const void *, size_t);
 extern void _libmd_md5block_bmi1(MD5_CTX *, const void *, size_t);
+extern void _libmd_md5block_avx512(MD5_CTX *, const void *, size_t);
 
 DEFINE_UIFUNC(, void, _libmd_md5block, (MD5_CTX *, const void *, size_t))
 {
+	/*
+	 * AVX-512 would need to be turned on first in the kernel
+	 * and that's too expensive; the BMI1 kernel is plenty fast
+	 * and doesn't require any special registers to run.
+	 */
+#ifndef _KERNEL
+	if ((cpu_stdext_feature & (CPUID_STDEXT_AVX512F | CPUID_STDEXT_AVX512VL))
+	    == (CPUID_STDEXT_AVX512F | CPUID_STDEXT_AVX512VL))
+		return (_libmd_md5block_avx512);
+#endif
+
 	if (cpu_stdext_feature & CPUID_STDEXT_BMI1)
 		return (_libmd_md5block_bmi1);
 	else
