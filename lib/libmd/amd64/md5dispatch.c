0ed91037ca24a6fc461d3e842e191b7fbe490ad8
Robert Clausecker <fuz@FreeBSD.org>
2024-06-20 08:15:23 -0400

    lib/libmd/amd64: add BMI1 implementation for amd64
    This implementation leverages the ANDN (and-not) instruction
    added with the BMI1 extension for amd64.  This improves
    performance by another 11%.
    
    pre     17.0s (602 MB/s)
    generic 18.8s (545 MB/s)
    scalar  13.4s (764 MB/s)
    bmi1    12.0s (853 MB/s)

---
+++ lib/libmd/amd64/md5dispatch.c
@@ -0,0 +1,22 @@
+/*-
+ * Copyright (c) 2024 Robert Clausecker <fuz@freebsd.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+// #include <stdint.h>
+#include <machine/specialreg.h>
+#include <sys/types.h>
+#include <md5.h>
+#include <x86/ifunc.h>
+
+extern void md5block_baseline(MD5_CTX *, const void *, size_t);
+extern void md5block_bmi1(MD5_CTX *, const void *, size_t);
+
+DEFINE_UIFUNC(, void, md5block, (MD5_CTX *, const void *, size_t))
+{
+	if (cpu_stdext_feature & CPUID_STDEXT_BMI1)
+		return (md5block_bmi1);
+	else
+		return (md5block_baseline);
+}
